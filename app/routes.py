7e8fe450b7e1e8ddc43a04b6cd1f3d2ebcfff0bf
titswort <titswort@oregonstate.edu>
2021-01-07 11:03:22 -0800

    template impl

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # see this variable
+    variable = {'pointer': 'value'}
+    # variable store for a loop
+    var_stores = [
+        {
+            'parameter1': {'pointer': '1'},
+            'parameter2': 'text1'
+        },
+        {
+            'parameter1': {'pointer': '2'},
+            'parameter2': 'text2'
+        }
+    ]
+    # notice the variable inclusions in list form on the render_template() function
+    return render_template('index.html', title='Home', variable=variable, var_stores=var_stores)
