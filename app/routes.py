c2bb50b04548ccea693c75873ec469c4c71182f6
titswort <titswort@oregonstate.edu>
2021-01-07 13:19:32 -0800

    login webform

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,12 @@ def index():
     ]
     # notice the variable inclusions in list form on the render_template() function
     return render_template('index.html', title='Home', variable=variable, var_stores=var_stores)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index')) #This will send a logged in user to the given route
+    return render_template('login.html', title='Sign In', form=form)
