01274f1c19a022fd5884f32d292c1fdc1e3a9668
titswort <titswort@oregonstate.edu>
2021-03-13 14:20:19 -0800

    fix main file

--- cs340.py
+++ cs340.py
@@ -1,9 +1,9 @@
 from app import app, db
-from app.models import Mutual_Funds, Stocks, User, Portfolios
+from app.models import Mutual_Funds, Stocks, User, Portfolios, Sectors, Holdings
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Portfolios': Portfolios, 'Mutual_Funds': Mutual_Funds, 'Stocks': Stocks}
+    return {'db': db, 'User': User, 'Portfolios': Portfolios, 'Mutual_Funds': Mutual_Funds, 'Stocks': Stocks, 'Sectors': Sectors, 'Holdings': Holdings}
 
 if __name__ == '__main__':
     app.run(host='flip2.engr.oregonstate.edu', port=12323, use_reloader=True, debug=False)
